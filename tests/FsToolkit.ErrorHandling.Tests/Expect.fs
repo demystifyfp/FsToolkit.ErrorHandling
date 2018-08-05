99bb523ff967408560ebdc01404ecaf30b597c52
Tamizhvendan S <tamizh88@gmail.com>
2018-08-05 21:28:44 +0530

    add apply tests

--- tests/FsToolkit.ErrorHandling.Tests/Expect.fs
+++ tests/FsToolkit.ErrorHandling.Tests/Expect.fs
@@ -1,7 +1,7 @@
 module Expect
 
 open Expecto
-let hasOkValuePredicate x f =
+let hasOkValuePredicate f x =
   match x with
   | Result.Ok v -> 
     if f v then 
@@ -11,11 +11,19 @@ let hasOkValuePredicate x f =
   | Result.Error x -> 
     Tests.failtestf "Expected Ok, was Error(%A)." x
 
-let hasErrorValue x v =
+let hasErrorValue v x =
   match x with
   | Ok x -> 
     Tests.failtestf "Expected Error, was Ok(%A)." x
   | Error x when x = v -> ()
   | Error x -> 
     Tests.failtestf "Expected Error(%A), was Error(%A)." v x
+
+let hasOkValue v x =
+  match x with
+  | Ok x when x = v -> ()
+  | Ok x ->
+    Tests.failtestf "Expected Ok(%A), was Ok(%A)." v x
+  | Error x -> 
+    Tests.failtestf "Expected Ok, was Error(%A)." x
 
--- tests/FsToolkit.ErrorHandling.Tests/Result.fs
+++ tests/FsToolkit.ErrorHandling.Tests/Result.fs
@@ -10,8 +10,10 @@ let lng = 80.237617
 
 let validLat = Latitude.TryCreate lat
 let invalidLat = Latitude.TryCreate 200.
+let invalidLatMsg = "200.0 is a invalid latitude value"
 let validLng = Longitude.TryCreate lng
 let invalidLng = Longitude.TryCreate 200.
+let invalidLngMsg = "200.0 is a invalid longitude value"
 let validTweet = Tweet.TryCreate "Hello, World!"
 
 let emptyInvalidTweet = Tweet.TryCreate ""
@@ -22,22 +24,22 @@ let map2Tests =
   testList "Result.map2 Tests" [
     testCase "map2 with two ok parts" <| fun _ ->
       let location = Result.map2 location validLat validLng
-      (fun location -> 
+      ((fun location -> 
         location.Latitude.Value = lat && 
-        location.Longitude.Value = lng)
-      |> Expect.hasOkValuePredicate location 
+        location.Longitude.Value = lng), location)
+      ||> Expect.hasOkValuePredicate 
 
     testCase "map2 with one Error and one Ok parts" <| fun _ -> 
-      let location = Result.map2 location invalidLat validLng
-      Expect.hasErrorValue location "200.0 is a invalid latitude value"
+      Result.map2 location invalidLat validLng
+      |> Expect.hasErrorValue invalidLatMsg
     
     testCase "map2 with one Ok and one Error parts" <| fun _ ->
-      let location = Result.map2 location validLat invalidLng
-      Expect.hasErrorValue location "200.0 is a invalid longitude value"
+      Result.map2 location validLat invalidLng
+      |> Expect.hasErrorValue invalidLngMsg
 
     testCase "map2 with two Error parts" <| fun _ -> 
-      let location = Result.map2 location invalidLat invalidLng
-      Expect.hasErrorValue location "200.0 is a invalid latitude value"
+      Result.map2 location invalidLat invalidLng
+      |> Expect.hasErrorValue invalidLatMsg
   ]
 
 
@@ -47,26 +49,40 @@ let map3Tests =
     testCase "map3 with three ok parts" <| fun _ ->
       
       let post = Result.map3 post validLat validLng validTweet
-      (fun post -> 
+      ((fun post -> 
         post.Location.Latitude.Value = lat && 
         post.Location.Longitude.Value = lng && 
-        post.Tweet.Value = "Hello, World!")
-      |> Expect.hasOkValuePredicate post 
+        post.Tweet.Value = "Hello, World!"), post)
+      ||> Expect.hasOkValuePredicate 
 
     testCase "map3 with (Error, Ok, Ok)" <| fun _ ->
-      let post = Result.map3 post invalidLat validLng validTweet
-      Expect.hasErrorValue post "200.0 is a invalid latitude value"
+      Result.map3 post invalidLat validLng validTweet
+      |> Expect.hasErrorValue invalidLatMsg
     
     testCase "map3 with (Ok, Error, Ok)" <| fun _ ->
-      let post = Result.map3 post validLat invalidLng validTweet
-      Expect.hasErrorValue post "200.0 is a invalid longitude value"
+      Result.map3 post validLat invalidLng validTweet
+      |> Expect.hasErrorValue invalidLngMsg
 
 
     testCase "map3 with (Ok, Ok, Error)" <| fun _ ->
-      let post = Result.map3 post validLat validLng emptyInvalidTweet
-      Expect.hasErrorValue post "Tweet shouldn't be empty"
+      Result.map3 post validLat validLng emptyInvalidTweet
+      |> Expect.hasErrorValue "Tweet shouldn't be empty"
     
     testCase "map3 with (Error, Error, Error)" <| fun _ ->
-      let post = Result.map3 post invalidLat invalidLng emptyInvalidTweet
-      Expect.hasErrorValue post "200.0 is a invalid latitude value"
+      Result.map3 post invalidLat invalidLng emptyInvalidTweet
+      |> Expect.hasErrorValue invalidLatMsg
+  ]
+
+[<Tests>]
+let applyTests =
+  let inc a = a + 1
+
+  testList "Result.apply tests" [
+    testCase "apply with Ok" <| fun _ ->
+      Result.apply (Ok inc) (Ok 1)
+      |> Expect.hasOkValue 2
+    
+    testCase "apply with Error" <| fun _ ->
+      Result.apply (Ok inc) (Error 1)
+      |> Expect.hasErrorValue 1
   ]
