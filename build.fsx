39a1d28a39b8b55ad71ab8fa1bf7de2ad1cf6a29
Tamizhvendan S <tamizh88@gmail.com>
2018-08-10 10:17:30 +0530

    fix run tests build target

--- build.fsx
+++ build.fsx
@@ -4,16 +4,18 @@
 open Fake.Core
 open Fake.IO
 open Fake.DotNet
+open Fake.IO.FileSystemOperators
+open Fake.Core.TargetOperators
+open Fake.DotNet.Testing
+open Fake.IO.Globbing.Operators
 
 let configuration = "Release"
 let solutionFile = "FsToolkit.ErrorHandling.sln"
-let buildConfiguration = 
-  Environment.environVarOrDefault "configuration" configuration
-  |> DotNet.Custom 
 
 Target.create "Clean" (fun _ ->
   Shell.cleanDirs ["bin"]
 )
+
 
 
 Target.create "Build" (fun _ ->
@@ -31,11 +33,54 @@ Target.create "Build" (fun _ ->
   MSBuild.build setParams solutionFile
 )
 
-open Fake.Core.TargetOperators
+let runTestAssembly setParams testAssembly =
+  let exitCode =
+    let fakeStartInfo testAssembly (args : Expecto.Params) =
+      let workingDir =
+        if String.isNotNullOrEmpty args.WorkingDirectory
+        then args.WorkingDirectory else Fake.IO.Path.getDirectory testAssembly
+      (fun (info: ProcStartInfo) ->
+        { info with 
+            FileName = "dotnet"
+            Arguments = sprintf "%s %s" testAssembly (string args)
+            WorkingDirectory = workingDir } )
+
+    let execWithExitCode testAssembly argsString timeout = 
+      Process.execSimple (fakeStartInfo testAssembly argsString) timeout
+
+    execWithExitCode testAssembly (setParams Expecto.Params.DefaultParams) System.TimeSpan.MaxValue
+
+  testAssembly, exitCode
+
+let runTests setParams testAssemblies =
+  let details = testAssemblies |> String.separated ", "
+  use __ = Trace.traceTask "Expecto" details
+  let res =
+    testAssemblies
+    |> Seq.map (runTestAssembly setParams)
+    |> Seq.filter( snd >> (<>) 0)
+    |> Seq.toList
+
+  match res with
+  | [] -> ()
+  | failedAssemblies ->
+      failedAssemblies
+      |> List.map (fun (testAssembly,exitCode) -> 
+          sprintf "Expecto test of assembly '%s' failed. Process finished with exit code %d." testAssembly exitCode )
+      |> String.concat System.Environment.NewLine
+      |> Fake.Testing.Common.FailedTestsException |> raise
+  __.MarkSuccess()
+
+let testAssemblies = "tests/**/bin" </> configuration </> "**" </> "*Tests.dll"
+
+Target.create "RunTests" (fun _ ->
+  runTests id (!! testAssemblies)
+)
 
 // *** Define Dependencies ***
 "Clean"
   ==> "Build"
+  ==> "RunTests"
 
 // *** Start Build ***
-Target.runOrDefault "Build"
+Target.runOrDefault "RunTests"
