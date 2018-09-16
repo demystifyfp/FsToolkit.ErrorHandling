4dd4bade77d26369408f9d1e576dccd01da9a1bc
Tamizhvendan S <tamizh88@gmail.com>
2018-09-16 16:37:32 +0530

    Bump version to 0.0.4

--- src/FsToolkit.ErrorHandling/AssemblyInfo.fs
+++ src/FsToolkit.ErrorHandling/AssemblyInfo.fs
@@ -5,8 +5,8 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FsToolkit.ErrorHandling")>]
 [<assembly: AssemblyProductAttribute("FsToolkit.ErrorHandling")>]
 [<assembly: AssemblyDescriptionAttribute("An opinionated error handling library for F#")>]
-[<assembly: AssemblyVersionAttribute("0.0.3")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.3")>]
+[<assembly: AssemblyVersionAttribute("0.0.4")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.4")>]
 [<assembly: AssemblyConfigurationAttribute("Release")>]
 do ()
 
@@ -14,6 +14,6 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "FsToolkit.ErrorHandling"
     let [<Literal>] AssemblyProduct = "FsToolkit.ErrorHandling"
     let [<Literal>] AssemblyDescription = "An opinionated error handling library for F#"
-    let [<Literal>] AssemblyVersion = "0.0.3"
-    let [<Literal>] AssemblyFileVersion = "0.0.3"
+    let [<Literal>] AssemblyVersion = "0.0.4"
+    let [<Literal>] AssemblyFileVersion = "0.0.4"
     let [<Literal>] AssemblyConfiguration = "Release"
