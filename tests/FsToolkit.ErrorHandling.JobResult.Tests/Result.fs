082165e27a5c6d7f55f5ed9b5ec9463c9a535eb2
Christer van der Meeren <cmeeren@protonmail.com>
2019-05-30 09:10:04 +0200

    Add tests

---
+++ tests/FsToolkit.ErrorHandling.JobResult.Tests/Result.fs
@@ -0,0 +1,20 @@
+﻿module Result
+
+open Hopac
+open Expecto
+open FsToolkit.ErrorHandling
+
+
+[<Tests>]
+let sequenceJobTests =
+  testList "sequenceJob Tests" [
+    testCase "sequenceJob returns the job value if Ok" <| fun _ ->
+      let resJob = job { return "foo" } |> Ok
+      let value = resJob |> Result.sequenceJob |> run
+      Expect.equal value (Ok "foo") ""
+
+    testCase "sequenceJob returns the error value if Error" <| fun _ ->
+      let resJob = Error "foo"
+      let value = resJob |> Result.sequenceJob |> run
+      Expect.equal value (Error "foo") ""
+  ]
