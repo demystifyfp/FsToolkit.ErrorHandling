45760798f725b7c926aa3a3b3ece5c7466408ea2
Jimmy Byrd <jimmybyrd@gmail.com>
2021-12-01 10:29:40 -0500

    Seq.sequenceResultM returns seq (#159)

--- src/FsToolkit.ErrorHandling/Seq.fs
+++ src/FsToolkit.ErrorHandling/Seq.fs
@@ -3,13 +3,13 @@ namespace FsToolkit.ErrorHandling
 [<RequireQualifiedAccess>]
 module Seq =
 
-    let sequenceResultM (xs: seq<Result<'t, 'e>>) : Result<'t list, 'e> =
+    let sequenceResultM (xs: seq<Result<'t, 'e>>) : Result<'t seq, 'e> =
         let rec loop xs ts =
             match Seq.tryHead xs with
             | Some x ->
                 x
                 |> Result.bind (fun t -> loop (Seq.tail xs) (t :: ts))
-            | None -> Ok(List.rev ts)
+            | None -> Ok(List.rev ts |> List.toSeq)
 
         // Seq.cache prevents double evaluation in Seq.tail
         loop (Seq.cache xs) []
