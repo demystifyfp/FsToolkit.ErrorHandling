fb63e8707402320807255f6fc7e839c0a6a8727d
Jimmy Byrd <jimmybyrd@gmail.com>
2022-02-06 13:25:15 -0500

    Adds InlineIfLambda variants

--- benchmarks/Benchmarks.fs
+++ benchmarks/Benchmarks.fs
@@ -3,10 +3,36 @@
 open System
 open BenchmarkDotNet
 open BenchmarkDotNet.Attributes
-open FsToolkit.ErrorHandling
+// open FsToolkit.ErrorHandling
 
 module Result =
+    module Normal =
+
+        let either okF errorF x =
+            match x with
+            | Ok x -> okF x
+            | Error err -> errorF err
+
+        let eitherMap okF errorF x =
+            either (okF >> Result.Ok) (errorF >> Result.Error) x
+
+        let apply f x =
+            Result.bind (fun f' ->
+            Result.bind (f' >> Ok) x) f
+
+        let map2 f x y =
+            (apply (apply (Ok f) x) y)
+
     module Inlined =  
+
+        let inline either okF errorF x =
+            match x with
+            | Ok x -> okF x
+            | Error err -> errorF err
+
+        let inline eitherMap okF errorF x =
+            either (okF >> Result.Ok) (errorF >> Result.Error) x
+
         let inline bind f x =
             match x with
             | Ok x -> f x
@@ -17,6 +43,24 @@ module Result =
         let inline map2 f x y =
             (apply (apply (Ok f) x) y)
 
+    module InlinedLambda =
+        let inline either ([<InlineIfLambda>] okF) ([<InlineIfLambda>] errorF) x =
+            match x with
+            | Ok x -> okF x
+            | Error err -> errorF err
+
+        let inline eitherMap ([<InlineIfLambda>] okF) ([<InlineIfLambda>] errorF) x =
+            either (okF >> Result.Ok) (errorF >> Result.Error) x
+
+        let inline bind ([<InlineIfLambda>] f) x =
+            match x with
+            | Ok x -> f x
+            | Error e -> Error e
+        let inline apply (f) x =
+            bind (fun f' ->
+                bind (f' >> Ok) x) f
+        let inline map2 ([<InlineIfLambda>] f) x y =
+            (apply (apply (Ok f) x) y)
     module Alt = 
         let eitherMap okF errorF x =
             match x with
@@ -52,7 +96,26 @@ module Result =
                 | Ok x, Ok y -> f x y |> Ok
                 | Error e as z, _ -> Error e
                 | _, Error e -> Error e
+        module InlinedLambda =
 
+            let inline eitherMap ([<InlineIfLambda>] okF) ([<InlineIfLambda>] errorF)  x =
+                match x with
+                | Ok x -> okF x |> Ok
+                | Error e -> errorF e |> Error
+
+            let inline apply f x =
+                match f,x with
+                | Ok f, Ok x -> f x |> Ok
+                | Error e, _ -> Error e
+                | _ , Error e -> Error e
+
+            let inline map2 ([<InlineIfLambda>] f) x y =
+                match x, y with
+                | Ok x, Ok y -> f x y |> Ok
+                | Error e as z, _ -> Error e
+                | _, Error e -> Error e
+
+[<MemoryDiagnoser>]
 type Benchmarks () =
 
     let okF x = x + 2
@@ -85,15 +148,32 @@ type Benchmarks () =
     // member self.IterationCleanup() =
     //     printfn "%s" "Iteration Cleanup"
     
-    // [<Benchmark>]
-    // member this.EitherMap () =
-    //     Ok 4
-    //     |> Result.eitherMap okF errorF 
+    [<Benchmark>]
+    member this.Result_Normal_EitherMap () =
+        Ok 4
+        |> Result.Normal.eitherMap okF errorF 
+    [<Benchmark>]
+    member this.Result_Inlined_EitherMap () =
+        Ok 4
+        |> Result.Inlined.eitherMap okF errorF 
+    [<Benchmark>]
+    member this.Result_InlinedLambda_EitherMap () =
+        Ok 4
+        |> Result.InlinedLambda.eitherMap okF errorF 
 
-    // [<Benchmark>]
-    // member this.AltEitherMap () = 
-    //     Ok 4
-    //     |> Result.Alt.eitherMap okF errorF 
+    [<Benchmark>]
+    member this.Result_Alt_EitherMap () = 
+        Ok 4
+        |> Result.Alt.eitherMap okF errorF 
+    [<Benchmark>]
+    member this.Result_Alt_Inilned_EitherMap () = 
+        Ok 4
+        |> Result.Alt.Inlined.eitherMap okF errorF 
+    [<Benchmark>]
+    member this.Result_Alt_InilnedLambda_EitherMap () = 
+        Ok 4
+        |> Result.Alt.InlinedLambda.eitherMap okF errorF 
+    
     
     // [<Benchmark>]
     // member this.ResultApply () : Result<int,int> =
@@ -109,23 +189,34 @@ type Benchmarks () =
 
     
     [<Benchmark>]
-    member this.ResultMap2 ()  = 
-       fun () -> Result.map2 add (Ok 1) (Ok 2) : Result<int,int>
-       |> runXTimes 100
+    member this.Result_Normal_Map2 ()  = 
+        Result.Normal.map2 add (Ok 1) (Ok 2) : Result<int,int>
+    //    |> runXTimes 100
 
     [<Benchmark>]
-    member this.ResultAltMap2 () = 
+    member this.Result_Inlined_Map2 ()  = 
+        Result.Inlined.map2 add (Ok 1) (Ok 2) : Result<int,int>
+    //    |> runXTimes 100
+
+    [<Benchmark>]
+    member this.Result_InlinedLambda_Map2 ()  = 
+        Result.InlinedLambda.map2 add (Ok 1) (Ok 2) : Result<int,int>
+    //    |> runXTimes 100
+
+    [<Benchmark>]
+    member this.Result_Alt_Map2 () = 
         
-       fun () -> Result.Alt.map2 add (Ok 1) (Ok 2) : Result<int,int>
-       |> runXTimes 100
+        Result.Alt.map2 add (Ok 1) (Ok 2) : Result<int,int>
+    //    |> runXTimes 100
     
     [<Benchmark>]
-    member this.ResultInlinedMap2 ()  = 
-       fun () -> Result.Inlined.map2 add (Ok 1) (Ok 2) : Result<int,int>
-       |> runXTimes 100
+    member this.Result_Alt_Inlined_Map2 () = 
+        
+        Result.Alt.Inlined.map2 add (Ok 1) (Ok 2) : Result<int,int>
+    //    |> runXTimes 100
 
     [<Benchmark>]
-    member this.ResultAltInlinedMap2 () = 
+    member this.Result_Alt_InlinedLambda_Map2 () = 
         
-       fun () -> Result.Alt.Inlined.map2 add (Ok 1) (Ok 2) : Result<int,int>
-       |> runXTimes 100
+        Result.Alt.InlinedLambda.map2 add (Ok 1) (Ok 2) : Result<int,int>
+    //    |> runXTimes 100
