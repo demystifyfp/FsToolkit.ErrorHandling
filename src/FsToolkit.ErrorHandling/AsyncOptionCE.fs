7e9fdab83bc46d0e73e6bfb4c344463a84ecc5d6
Jimmy Byrd <jimmybyrd@gmail.com>
2022-02-20 11:49:56 -0500

    AsyncOptionCE using inlineiflambda

--- src/FsToolkit.ErrorHandling/AsyncOptionCE.fs
+++ src/FsToolkit.ErrorHandling/AsyncOptionCE.fs
@@ -7,51 +7,45 @@ open System
 module AsyncOptionCE =
     type AsyncOptionBuilder() =
 
-        member __.Return(value: 'T) : Async<_ option> = async.Return <| option.Return value
+        member inline _.Return(value: 'value) : Async<'value option> = async.Return <| option.Return value
 
-        member __.ReturnFrom(asyncResult: Async<_ option>) : Async<_ option> = asyncResult
+        member inline _.ReturnFrom(value: Async<'value option>) : Async<'value option> = value
 
-        member __.Zero() : Async<_ option> = async.Return <| option.Zero()
+        member inline _.Zero() : Async<unit option> = async.Return <| option.Zero()
 
-        member inline __.Bind(asyncResult: Async<_ option>, binder: 'T -> Async<_ option>) : Async<_ option> =
-            async {
-                let! result = asyncResult
+        member inline _.Bind(input: Async<'input option>,[<InlineIfLambda>] binder: 'input -> Async<'output option>) : Async<'output option> =
+            AsyncOption.bind binder input
 
-                match result with
-                | Some x -> return! binder x
-                | None -> return None
-            }
+        member inline _.Delay(generator: unit -> Async<'value option>) : Async<'value option> = async.Delay generator
 
-        member __.Delay(generator: unit -> Async<_ option>) : Async<_ option> = async.Delay generator
-
-        member this.Combine(computation1: Async<_ option>, computation2: Async<_ option>) : Async<_ option> =
+        member inline this.Combine(computation1: Async<_ option>, computation2: Async<_ option>) : Async<_ option> =
             this.Bind(computation1, (fun () -> computation2))
 
-        member __.TryWith
+        member inline _.TryWith
             (
-                computation: Async<_ option>,
-                handler: System.Exception -> Async<_ option>
+                computation: Async<'value option>,
+                [<InlineIfLambda>] handler: System.Exception -> Async<'value option>
             ) : Async<_ option> =
             async.TryWith(computation, handler)
 
-        member __.TryFinally(computation: Async<_ option>, compensation: unit -> unit) : Async<_ option> =
+        member inline _.TryFinally(computation: Async<'value option>, [<InlineIfLambda>] compensation: unit -> unit) : Async<'value option> =
             async.TryFinally(computation, compensation)
 
-        member __.Using(resource: 'T :> IDisposable, binder: 'T -> Async<_ option>) : Async<_ option> =
-            __.TryFinally(
+        member inline this.Using(resource: 'disposable :> IDisposable, [<InlineIfLambda>] binder: 'disposable -> Async<'output option>) : Async<'output option> =
+            this.TryFinally(
                 (binder resource),
                 (fun () ->
                     if not <| obj.ReferenceEquals(resource, null) then
                         resource.Dispose())
             )
 
-        member this.While(guard: unit -> bool, computation: Async<_ option>) : Async<_ option> =
+        member this.While(guard: unit -> bool, computation: Async<unit option>) : Async<unit option> =
             if not <| guard () then
                 this.Zero()
             else
                 this.Bind(computation, (fun () -> this.While(guard, computation)))
 
-        member this.For(sequence: #seq<'T>, binder: 'T -> Async<_ option>) : Async<_ option> =
+        member inline this.For(sequence: #seq<'input>, binder: 'input -> Async<unit option>) : Async<unit option> =
             this.Using(
                 sequence.GetEnumerator(),
                 fun enum -> this.While(enum.MoveNext, this.Delay(fun () -> binder enum.Current))
@@ -62,13 +56,13 @@ module AsyncOptionCE =
         ///
         /// See https://stackoverflow.com/questions/35286541/why-would-you-use-builder-source-in-a-custom-computation-expression-builder
         /// </summary>
-        member inline _.Source(async: Async<_ option>) : Async<_ option> = async
+        member inline _.Source(async: Async<'value option>) : Async<'value option> = async
 
 #if !FABLE_COMPILER
         /// <summary>
         /// Method lets us transform data types into our internal representation.
         /// </summary>
-        member inline _.Source(task: Task<_ option>) : Async<_ option> = task |> Async.AwaitTask
+        member inline _.Source(task: Task<'value option>) : Async<'value option> = task |> Async.AwaitTask
 #endif
 
     let asyncOption = AsyncOptionBuilder()
@@ -83,20 +77,20 @@ module AsyncOptionCEExtensions =
         /// <summary>
         /// Needed to allow `for..in` and `for..do` functionality
         /// </summary>
-        member inline __.Source(s: #seq<_>) = s
+        member inline __.Source(s: #seq<_>) : #seq<_> = s
 
         /// <summary>
         /// Method lets us transform data types into our internal representation.
         /// </summary>
-        member inline __.Source(r: 't option) = Async.singleton r
+        member inline __.Source(r: 'value option) : Async<option<'value>> = Async.singleton r
         /// <summary>
         /// Method lets us transform data types into our internal representation.
         /// </summary>
-        member inline __.Source(a: Async<'t>) = a |> Async.map Some
+        member inline __.Source(a: Async<'value>) : Async<option<'value>> = a |> Async.map Some
 
 #if !FABLE_COMPILER
         /// <summary>
         /// Method lets us transform data types into our internal representation.
         /// </summary>
-        member inline __.Source(a: Task<'t>) = a |> Async.AwaitTask |> Async.map Some
+        member inline __.Source(a: Task<'value>) : Async<option<'value>> = a |> Async.AwaitTask |> Async.map Some
 #endif
