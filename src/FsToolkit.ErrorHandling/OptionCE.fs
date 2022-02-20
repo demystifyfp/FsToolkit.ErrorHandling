93a3e61a047504035cfb0a3289eff90caaf4f3d8
Jimmy Byrd <jimmybyrd@gmail.com>
2022-02-20 16:46:48 -0500

    Option InlineIfLambda

--- src/FsToolkit.ErrorHandling/OptionCE.fs
+++ src/FsToolkit.ErrorHandling/OptionCE.fs
@@ -5,37 +5,57 @@ module OptionCE =
     open System
 
     type OptionBuilder() =
-        member inline _.Return(x) = Some x
+        member inline _.Return(x: 'value) : 'value option = Some x
 
-        member inline _.ReturnFrom(m: 'T option) = m
+        member inline _.ReturnFrom(m: 'value option) : 'value option = m
 
-        member inline _.Bind(m: 'a option, f: 'a -> 'b option) = Option.bind f m
+        member inline _.Bind
+            (
+                input: 'input option,
+                [<InlineIfLambda>] binder: 'input -> 'output option
+            ) : 'output option =
+            Option.bind binder input
 
         // Could not get it to work solely with Source. In loop cases it would potentially match the #seq overload and ask for type annotation
-        member this.Bind(m: 'a when 'a: null, f: 'a -> 'b option) = this.Bind(m |> Option.ofObj, f)
+        member inline this.Bind
+            (
+                m: 'input when 'input: null,
+                [<InlineIfLambda>] binder: 'input -> 'output option
+            ) : 'output option =
+            this.Bind(Option.ofObj m, binder)
 
-        member inline this.Zero() = this.Return()
+        member inline this.Zero() : unit option = this.Return()
 
-        member inline _.Combine(m, f) = Option.bind f m
-        member inline this.Combine(m1: _ option, m2: _ option) = this.Bind(m1, (fun () -> m2))
+        member inline _.Combine
+            (
+                m: 'input option,
+                [<InlineIfLambda>] binder: 'input -> 'output option
+            ) : 'output option =
+            Option.bind binder m
 
-        member inline _.Delay(f: unit -> _) = f
+        member inline this.Combine(m1: unit option, m2: 'output option) : 'output option = this.Bind(m1, (fun () -> m2))
 
-        member inline _.Run(f) = f ()
+        member inline _.Delay([<InlineIfLambda>] delayer: unit -> 'value option) : (unit -> 'value option) = delayer
 
-        member inline this.TryWith(m, h) =
+        member inline _.Run([<InlineIfLambda>] delayed) = delayed ()
+
+        member inline this.TryWith([<InlineIfLambda>] computation, handler) : 'value =
             try
-                this.Run m
+                this.Run computation
             with
-            | e -> h e
+            | e -> handler e
 
-        member inline this.TryFinally(m, compensation) =
+        member inline this.TryFinally([<InlineIfLambda>] computation, compensation) =
             try
-                this.Run m
+                this.Run computation
             finally
                 compensation ()
 
-        member inline this.Using(resource: 'T :> IDisposable, binder) : _ option =
+        member inline this.Using
+            (
+                resource: 'disposable :> IDisposable,
+                [<InlineIfLambda>] binder: 'disposable -> 'value option
+            ) : 'value option =
             this.TryFinally(
                 (fun () -> binder resource),
                 (fun () ->
@@ -43,60 +63,72 @@ module OptionCE =
                         resource.Dispose())
             )
 
-        member this.While(guard: unit -> bool, generator: unit -> _ option) : _ option =
-            if not <| guard () then
-                this.Zero()
-            else
-                this.Bind(this.Run generator, (fun () -> this.While(guard, generator)))
+        member inline this.While
+            (
+                [<InlineIfLambda>] guard: unit -> bool,
+                [<InlineIfLambda>] computation: unit -> unit option
+            ) : unit option =
+            if guard () then
+                let mutable whileBuilder = Unchecked.defaultof<_>
 
-        member inline this.For(sequence: #seq<'T>, binder: 'T -> _ option) : _ option =
+                whileBuilder <-
+                    fun () ->
+                        this.Bind(
+                            this.Run computation,
+                            (fun () ->
+                                if guard () then
+                                    this.Run whileBuilder
+                                else
+                                    this.Zero())
+                        )
+
+                this.Run whileBuilder
+            else
+                this.Zero()
+
+        member inline this.For(sequence: #seq<'value>, [<InlineIfLambda>] binder: 'value -> unit option) : unit option =
             this.Using(
                 sequence.GetEnumerator(),
                 fun enum -> this.While(enum.MoveNext, this.Delay(fun () -> binder enum.Current))
             )
 
-        member inline _.BindReturn(x, f) = Option.map f x
-        member inline _.BindReturn(x, f) = x |> Option.ofObj |> Option.map f
-        member inline _.MergeSources(option1, option2) = Option.zip option1 option2
+        member inline _.BindReturn(input : 'input option,  [<InlineIfLambda>] mapper : 'input -> 'output) : 'output option = Option.map mapper input
+        member inline _.BindReturn(x : 'input, [<InlineIfLambda>] f : 'input -> 'output) : 'output option = Option.map f (Option.ofObj x)
+        member inline _.MergeSources(option1 : 'left option, option2 : 'right option) : ('left * 'right) option = Option.zip option1 option2
 
         /// <summary>
         /// Method lets us transform data types into our internal representation.  This is the identity method to recognize the self type.
         ///
         /// See https://stackoverflow.com/questions/35286541/why-would-you-use-builder-source-in-a-custom-computation-expression-builder
         /// </summary>
-        member inline _.Source(result: _ option) : _ option = result
+        member inline _.Source(result: 'value option) : 'value option = result
 
 
         // /// <summary>
         // /// Method lets us transform data types into our internal representation.
         // /// </summary>
-        member inline _.Source(vopt: ValueOption<_>) : Option<_> = vopt |> Option.ofValueOption
+        member inline _.Source(vopt: 'value voption) : 'value option = Option.ofValueOption vopt
 
     let option = OptionBuilder()
 
 [<AutoOpen>]
-// Having members as extensions gives them lower priority in
-// overload resolution and allows skipping more type annotations.
 module OptionExtensionsLower =
     type OptionBuilder with
-        member inline _.Source(nullableObj: 'a when 'a: null) = nullableObj |> Option.ofObj
-        member inline _.Source(m: string) = m |> Option.ofObj
+        member inline _.Source(nullableObj: 'value when 'value: null) : 'value option = Option.ofObj nullableObj
+        member inline _.Source(m: string) : string option = Option.ofObj m
 
-        member inline _.MergeSources(nullableObj1, option2) =
+        member inline _.MergeSources(nullableObj1 : 'left, option2 : 'right option) : ('left * 'right) option =
             Option.zip (Option.ofObj nullableObj1) option2
 
 
-        member inline _.MergeSources(option1, nullableObj2) =
+        member inline _.MergeSources(option1 : 'left option, nullableObj2 : 'right) : ('left * 'right) option =
             Option.zip (option1) (Option.ofObj nullableObj2)
 
 
-        member inline _.MergeSources(nullableObj1, nullableObj2) =
+        member inline _.MergeSources(nullableObj1 : 'left, nullableObj2 : 'right) : ('left * 'right) option =
             Option.zip (Option.ofObj nullableObj1) (Option.ofObj nullableObj2)
 
 [<AutoOpen>]
-// Having members as extensions gives them lower priority in
-// overload resolution and allows skipping more type annotations.
-// The later declared, the higher than previous extension methods, this is magic
 module OptionExtensions =
     open System
 
@@ -104,9 +136,9 @@ module OptionExtensions =
         /// <summary>
         /// Needed to allow `for..in` and `for..do` functionality
         /// </summary>
-        member inline _.Source(s: #seq<_>) = s
+        member inline _.Source(s: #seq<'value>) : #seq<'value> = s
 
         // /// <summary>
         // /// Method lets us transform data types into our internal representation.
         // /// </summary>
-        member inline _.Source(nullable: Nullable<'a>) : 'a option = nullable |> Option.ofNullable
+        member inline _.Source(nullable: Nullable<'value>) : 'value option = Option.ofNullable nullable
