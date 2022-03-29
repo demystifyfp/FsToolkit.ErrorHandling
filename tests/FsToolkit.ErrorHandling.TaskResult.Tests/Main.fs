2290d874385310e8a447d3d35dd01f17f576717c
Jimmy Byrd <jimmybyrd@gmail.com>
2022-03-29 12:45:39 -0400

    Adds native TaskResultCE

--- tests/FsToolkit.ErrorHandling.TaskResult.Tests/Main.fs
+++ tests/FsToolkit.ErrorHandling.TaskResult.Tests/Main.fs
@@ -1,10 +1,6 @@
 module FsToolkit.ErrorHandling.TaskResult.Tests
 
 open Expecto
-open FSharp.Control.Tasks
-open System.Threading.Tasks
-open System
-open FsToolkit.ErrorHandling
 
 [<EntryPoint>]
 let main argv =
