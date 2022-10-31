d8f60d35fa8ef31eaaa2bd5e76f98035c7f49ce4
Jimmy Byrd <jimmybyrd@gmail.com>
2022-10-31 14:14:16 -0400

    Adds applicative vs bind benchmarks

---
+++ benchmarks/ApplicativeTests.fs
@@ -0,0 +1,110 @@
+module ApplicativeTests
+
+
+open BenchmarkDotNet.Attributes
+open FsToolkit.ErrorHandling
+open System.Threading
+open System
+
+let successSlowSync (time : TimeSpan) =
+    Thread.Sleep time
+    Ok time.Ticks
+
+let errorSlowSync (time : TimeSpan) =
+    Thread.Sleep time
+    Error "failed"
+
+[<MemoryDiagnoser>]
+type Result_BindvsAndCEBenchmarks () =
+
+    member this.ValuesForDelay = [|
+        TimeSpan.FromMilliseconds (0.)
+        TimeSpan.FromMilliseconds (10.)
+        TimeSpan.FromMilliseconds (100.)
+    |]
+    // let this.delay = TimeSpan.FromMilliseconds 100.
+    [<ParamsSource("ValuesForDelay")>]
+    member val public delay =
+        TimeSpan.MinValue
+            with get,set
+
+
+    [<Benchmark(Baseline = true)>]
+    member this.All_Success_Bind()  =
+        result {
+            let! r1 = successSlowSync this.delay
+            let! r2 = successSlowSync this.delay
+            let! r3 = successSlowSync this.delay
+            return r1 + r2 + r3
+        }
+        |> ignore
+
+    [<Benchmark>]
+    member this.All_Success_And()  =
+        result {
+            let! r1 = successSlowSync this.delay
+            and! r2 = successSlowSync this.delay
+            and! r3 = successSlowSync this.delay
+            return r1 + r2 + r3
+        }
+        |> ignore
+
+    [<Benchmark>]
+    member this.Fail_First_Bind()  =
+        result {
+            let! r1 = errorSlowSync this.delay
+            let! r2 = successSlowSync this.delay
+            let! r3 = successSlowSync this.delay
+            return r1 + r2 + r3
+        }
+        |> ignore
+
+    [<Benchmark>]
+    member this.Fail_First_And()  =
+        result {
+            let! r1 = errorSlowSync this.delay
+            and! r2 = successSlowSync this.delay
+            and! r3 = successSlowSync this.delay
+            return r1 + r2 + r3
+        }
+        |> ignore
+
+    [<Benchmark>]
+    member this.Fail_Mid_Bind()  =
+        result {
+            let! r1 = successSlowSync this.delay
+            let! r2 = errorSlowSync this.delay
+            let! r3 = successSlowSync this.delay
+            return r1 + r2 + r3
+        }
+        |> ignore
+
+    [<Benchmark>]
+    member this.Fail_Mid_And()  =
+        result {
+            let! r1 = successSlowSync this.delay
+            and! r2 = errorSlowSync this.delay
+            and! r3 = successSlowSync this.delay
+            return r1 + r2 + r3
+        }
+        |> ignore
+
+    [<Benchmark>]
+    member this.Fail_Last_Bind()  =
+        result {
+            let! r1 = successSlowSync this.delay
+            let! r2 = successSlowSync this.delay
+            let! r3 = errorSlowSync this.delay
+            return r1 + r2 + r3
+        }
+        |> ignore
+
+    [<Benchmark>]
+    member this.Fail_Last_And()  =
+        result {
+            let! r1 = successSlowSync this.delay
+            and! r2 = successSlowSync this.delay
+            and! r3 = errorSlowSync this.delay
+            return r1 + r2 + r3
+        }
+        |> ignore
--- benchmarks/Program.fs
+++ benchmarks/Program.fs
@@ -5,16 +5,19 @@ open BenchmarkDotNet.Configs
 open BenchmarkDotNet.Jobs
 open BenchmarkDotNet.Environments
 open FsToolkit.ErrorHandling.Benchmarks
+open ApplicativeTests
 
 [<EntryPoint>]
 let main argv =
-    let cfg = 
+    let cfg =
         DefaultConfig.Instance
-            // .AddJob(Job.Default.WithRuntime(CoreRuntime.Core50)) 
+            // .AddJob(Job.Default.WithRuntime(CoreRuntime.Core50))
             .AddJob(Job.Default.WithRuntime(CoreRuntime.Core60))
     // BenchmarkRunner.Run<EitherMapBenchmarks>() |> ignore
-    BenchmarkRunner.Run<TaskResult_BindCEBenchmarks>(cfg) |> ignore
+    // BenchmarkRunner.Run<TaskResult_BindCEBenchmarks>(cfg) |> ignore
     // BenchmarkRunner.Run<BindSameBenchmarks>() |> ignore
-// 
-    
+
+    BenchmarkRunner.Run<Result_BindvsAndCEBenchmarks>(cfg) |> ignore
+//
+
     0 // return an integer exit code
