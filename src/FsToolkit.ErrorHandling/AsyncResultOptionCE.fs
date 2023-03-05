5ef88fd8a4c08612eb97f54bd83191757115db4d
Jimmy Byrd <jimmybyrd@gmail.com>
2023-03-05 18:40:49 -0500

    Making AsyncResultOption more usable

--- src/FsToolkit.ErrorHandling/AsyncResultOptionCE.fs
+++ src/FsToolkit.ErrorHandling/AsyncResultOptionCE.fs
@@ -1,10 +1,13 @@
 namespace FsToolkit.ErrorHandling
 
+open System
+open System.Threading.Tasks
+
 [<AutoOpen>]
 module AsyncResultOptionCE =
 
     type AsyncResultOptionBuilder() =
-        member inline _.Return(value: 'ok) : Async<Result<'ok option, 'error>> =
+        member inline _.Return(value: 'ok) : AsyncResultOption<'ok, 'error> =
             AsyncResultOption.retn value
 
         member inline _.ReturnFrom
@@ -14,9 +17,9 @@ module AsyncResultOptionCE =
 
         member inline _.Bind
             (
-                input: Async<Result<'okInput option, 'error>>,
-                [<InlineIfLambda>] binder: 'okInput -> Async<Result<'okOutput option, 'error>>
-            ) : Async<Result<'okOutput option, 'error>> =
+                input: AsyncResultOption<'okInput, 'error>,
+                [<InlineIfLambda>] binder: 'okInput -> AsyncResultOption<'okOutput, 'error>
+            ) : AsyncResultOption<'okOutput, 'error> =
             AsyncResultOption.bind binder input
 
         member inline _.Combine(aro1, aro2) =
@@ -25,4 +28,171 @@ module AsyncResultOptionCE =
 
         member inline _.Delay([<InlineIfLambda>] f: unit -> Async<'a>) : Async<'a> = async.Delay f
 
+        member inline _.Zero() = Async.singleton (Ok(option.Zero()))
+
+        member inline _.TryWith
+            (
+                computation: AsyncResultOption<'ok, 'error>,
+                [<InlineIfLambda>] handler: Exception -> AsyncResultOption<'ok, 'error>
+            ) : AsyncResultOption<'ok, 'error> =
+            async.TryWith(computation, handler)
+
+        member inline _.TryFinally
+            (
+                computation: AsyncResultOption<'ok, 'error>,
+                [<InlineIfLambda>] compensation: unit -> unit
+            ) : AsyncResultOption<'ok, 'error> =
+            async.TryFinally(computation, compensation)
+
+
+        member inline _.Using
+            (
+                resource: 'ok :> IDisposable,
+                [<InlineIfLambda>] binder: 'ok -> AsyncResultOption<'okOutput, 'error>
+            ) : AsyncResultOption<'okOutput, 'error> =
+            async.Using(resource, binder)
+
+
+        member inline this.While
+            (
+                [<InlineIfLambda>] guard: unit -> bool,
+                computation: AsyncResultOption<unit, 'error>
+            ) : AsyncResultOption<unit, 'error> =
+            if guard () then
+                let mutable whileAsync = Unchecked.defaultof<_>
+
+                whileAsync <-
+                    this.Bind(computation, (fun () -> if guard () then whileAsync else this.Zero()))
+
+                whileAsync
+            else
+                this.Zero()
+
+
+        member inline this.For
+            (
+                sequence: #seq<'ok>,
+                [<InlineIfLambda>] binder: 'ok -> AsyncResultOption<unit, 'error>
+            ) : AsyncResultOption<unit, 'error> =
+            this.Using(
+                sequence.GetEnumerator(),
+                fun enum ->
+                    this.While(
+                        (fun () -> enum.MoveNext()),
+                        this.Delay(fun () -> binder enum.Current)
+                    )
+            )
+
+        /// <summary>
+        /// Method lets us transform data types into our internal representation.  This is the identity method to recognize the self type.
+        ///
+        /// See https://stackoverflow.com/questions/35286541/why-would-you-use-builder-source-in-a-custom-computation-expression-builder
+        /// </summary>
+        member inline _.Source
+            (result: AsyncResultOption<'ok, 'error>)
+            : AsyncResultOption<'ok, 'error> =
+            result
+#if !FABLE_COMPILER
+
+        /// <summary>
+        /// Method lets us transform data types into our internal representation.  This is the identity method to recognize the self type.
+        ///
+        /// See https://stackoverflow.com/questions/35286541/why-would-you-use-builder-source-in-a-custom-computation-expression-builder
+        /// </summary>
+        member inline _.Source
+            (result: Task<Result<Option<'ok>, 'error>>)
+            : AsyncResultOption<'ok, 'error> =
+            result
+            |> Async.AwaitTask
+#endif
     let asyncResultOption = new AsyncResultOptionBuilder()
+
+
+[<AutoOpen>]
+module AsyncResultOptionCEExtensions =
+
+    type AsyncResultOptionBuilder with
+
+        /// <summary>
+        /// Needed to allow `for..in` and `for..do` functionality
+        /// </summary>
+        member inline _.Source(s: #seq<'value>) : #seq<'value> = s
+
+        /// <summary>
+        /// Method lets us transform data types into our internal representation.
+        /// </summary>
+        member inline _.Source(result: Result<'ok, 'error>) : AsyncResultOption<'ok, 'error> =
+            AsyncResultOption.ofResult result
+
+        /// <summary>
+        /// Method lets us transform data types into our internal representation.
+        /// </summary>
+        member inline this.Source(result: Choice<'ok, 'error>) : AsyncResultOption<'ok, 'error> =
+            this.Source(Result.ofChoice result)
+
+        /// <summary>
+        /// Method lets us transform data types into our internal representation.
+        /// </summary>
+        member inline this.Source(optional: Option<'ok>) : AsyncResultOption<'ok, 'error> =
+            AsyncResultOption.ofOption optional
+
+        /// <summary>
+        /// Method lets us transform data types into our internal representation.
+        /// </summary>
+        member inline this.Source(async: Async<'ok>) : AsyncResultOption<'ok, 'error> =
+            Async.map (Some >> Ok) async
+
+#if !FABLE_COMPILER
+        /// <summary>
+        /// Method lets us transform data types into our internal representation.
+        /// </summary>
+        member inline this.Source(async: Task<'ok>) : AsyncResultOption<'ok, 'error> =
+            async
+            |> Async.AwaitTask
+            |> Async.map (Some >> Ok)
+
+        /// <summary>
+        /// Method lets us transform data types into our internal representation.
+        /// </summary>
+        member inline this.Source(async: Task) : AsyncResultOption<unit, 'error> =
+            async
+            |> Async.AwaitTask
+            |> Async.map (Some >> Ok)
+#endif
+[<AutoOpen>]
+module AsyncResultOptionCEExtensionsHighPriority =
+
+    type AsyncResultOptionBuilder with
+
+        /// <summary>
+        /// Method lets us transform data types into our internal representation.
+        /// </summary>
+        member inline _.Source
+            (result: Async<Result<'ok, 'error>>)
+            : AsyncResultOption<'ok, 'error> =
+            AsyncResultOption.ofAsyncResult result
+
+        /// <summary>
+        /// Method lets us transform data types into our internal representation.
+        /// </summary>
+        member inline _.Source(result: Async<Option<'ok>>) : AsyncResultOption<'ok, 'error> =
+            AsyncResultOption.ofAsyncOption result
+
+
+#if !FABLE_COMPILER
+        /// <summary>
+        /// Method lets us transform data types into our internal representation.
+        /// </summary>
+        member inline _.Source(result: Task<Result<'ok, 'error>>) : AsyncResultOption<'ok, 'error> =
+            result
+            |> Async.AwaitTask
+            |> AsyncResultOption.ofAsyncResult
+
+        /// <summary>
+        /// Method lets us transform data types into our internal representation.
+        /// </summary>
+        member inline _.Source(result: Task<Option<'ok>>) : AsyncResultOption<'ok, 'error> =
+            result
+            |> Async.AwaitTask
+            |> AsyncResultOption.ofAsyncOption
+#endif
---
+++ tests/FsToolkit.ErrorHandling.Tests/AsyncResultOptionCE.fs
@@ -0,0 +1,610 @@
+module AsyncResultOptionCETests
+#if FABLE_COMPILER
+open Fable.Mocha
+#else
+open Expecto
+#endif
+open SampleDomain
+open TestHelpers
+open FsToolkit.ErrorHandling
+open FsToolkit.ErrorHandling.Operator.AsyncResultOption
+open System
+
+
+#if FABLE_COMPILER
+open Fable.Mocha
+#else
+open Expecto
+#endif
+open SampleDomain
+open TestData
+open TestHelpers
+open System.Threading.Tasks
+open FsToolkit.ErrorHandling
+
+let inline OkSome (value: 'a) = Ok(Some(value))
+let inline OkNone _ = Ok(None)
+
+let ``AsyncResultOptionCE return Tests`` =
+    testList "AsyncResultOptionCE  Tests" [
+        testCaseAsync "Return string"
+        <| async {
+            let data = "Foo"
+            let! actual = asyncResultOption { return data }
+            Expect.equal actual (OkSome data) "Should be ok"
+        }
+    ]
+
+
+let ``AsyncResultOptionCE return! Tests`` =
+    testList "AsyncResultOptionCE return! Tests" [
+        testCaseAsync "Return Ok Result"
+        <| async {
+            let innerData = "Foo"
+            let data = Ok innerData
+            let! actual = asyncResultOption { return! data }
+
+            Expect.equal actual (OkSome innerData) "Should be ok"
+        }
+        testCaseAsync "Return Ok Choice"
+        <| async {
+            let innerData = "Foo"
+            let data = Choice1Of2 innerData
+            let! actual = asyncResultOption { return! data }
+            Expect.equal actual (OkSome innerData) "Should be ok"
+        }
+        testCaseAsync "Return Some"
+        <| async {
+            let innerData = "Foo"
+            let data = Some innerData
+            let! actual = asyncResultOption { return! data }
+            Expect.equal actual (OkSome innerData) "Should be ok"
+        }
+
+        testCaseAsync "Return Ok AsyncResultOption"
+        <| async {
+            let innerData = "Foo"
+            let data = AsyncResultOption.retn innerData
+            let! actual = asyncResultOption { return! data }
+
+            Expect.equal actual (OkSome innerData) "Should be ok"
+        }
+
+        testCaseAsync "Return Ok AsyncResult"
+        <| async {
+            let innerData = "Foo"
+            let data = AsyncResult.retn innerData
+            let! actual = asyncResultOption { return! data }
+
+            Expect.equal actual (OkSome innerData) "Should be ok"
+        }
+        testCaseAsync "Return Ok AsyncOption"
+        <| async {
+            let innerData = "Foo"
+            let data = AsyncOption.retn innerData
+            let! actual = asyncResultOption { return! data }
+
+            Expect.equal actual (OkSome innerData) "Should be ok"
+        }
+        testCaseAsync "Return Async"
+        <| async {
+            let innerData = "Foo"
+            let! actual = asyncResultOption { return! Async.singleton innerData }
+
+            Expect.equal actual (OkSome innerData) "Should be ok"
+        }
+#if !FABLE_COMPILER
+
+        testCaseAsync "Return Ok TaskResultOption"
+        <| async {
+            let innerData = "Foo"
+            let data = Task.FromResult(Ok(Some innerData))
+            let! actual = asyncResultOption { return! data }
+
+            Expect.equal actual (OkSome innerData) "Should be ok"
+        }
+
+        testCaseAsync "Return Ok TaskResult"
+        <| async {
+            let innerData = "Foo"
+            let data = Task.FromResult(Ok innerData)
+            let! actual = asyncResultOption { return! data }
+
+            Expect.equal actual (OkSome innerData) "Should be ok"
+        }
+        testCaseAsync "Return Ok TaskOption"
+        <| async {
+            let innerData = "Foo"
+            let data = Task.FromResult(Some innerData)
+            let! actual = asyncResultOption { return! data }
+
+            Expect.equal actual (OkSome innerData) "Should be ok"
+        }
+        testCaseAsync "Return Task<'T>"
+        <| async {
+            let innerData = "Foo"
+            let! actual = asyncResultOption { return! Task.FromResult innerData }
+
+            Expect.equal actual (OkSome innerData) "Should be ok"
+        }
+        testCaseAsync "Return Task"
+        <| async {
+            let innerData = ()
+            let data: Task = Task.FromResult()
+            let! actual = asyncResultOption { return! data }
+
+            Expect.equal actual (OkSome innerData) "Should be ok"
+        }
+#endif
+
+
+    ]
+
+
+let ``AsyncResultOptionCE bind Tests`` =
+    testList "AsyncResultOptionCE bind Tests" [
+        testCaseAsync "Bind Ok Result.Ok"
+        <| async {
+            let innerData = "Foo"
+            let data = Result.Ok innerData
+
+            let! actual = asyncResultOption {
+                let! data = data
+                return data
+            }
+
+            Expect.equal actual (OkSome innerData) "Should be ok"
+        }
+        testCaseAsync "Bind Ok Result.Error"
+        <| async {
+            let innerData = "Foo"
+            let data = Result.Error innerData
+
+            let! actual = asyncResultOption {
+                let! data = data
+                return data
+            }
+
+            Expect.equal actual (Error innerData) "Should be ok"
+        }
+        testCaseAsync "Bind Ok Choice1Of2"
+        <| async {
+            let innerData = "Foo"
+            let data = Choice1Of2 innerData
+
+            let! actual = asyncResultOption {
+                let! data = data
+                return data
+            }
+
+            Expect.equal actual (OkSome innerData) "Should be ok"
+        }
+
+        testCaseAsync "Bind Ok Choice2Of2"
+        <| async {
+            let innerData = "Foo"
+            let data = Choice2Of2 innerData
+
+            let! actual = asyncResultOption {
+                let! data = data
+                return data
+            }
+
+            Expect.equal actual (Error innerData) "Should be ok"
+        }
+
+        testCaseAsync "Bind Option.None"
+        <| async {
+            let innerData = "Foo"
+            let data = None
+
+            let! actual = asyncResultOption {
+                let! data = data
+                return data
+            }
+
+            Expect.equal actual (OkNone innerData) "Should be ok"
+        }
+
+
+        testCaseAsync "Bind Option.Some"
+        <| async {
+            let innerData = "Foo"
+            let data = Some innerData
+
+            let! actual = asyncResultOption {
+                let! data = data
+                return data
+            }
+
+            Expect.equal actual (OkSome innerData) "Should be ok"
+        }
+
+        testCaseAsync "Bind Ok AsyncResult"
+        <| async {
+            let innerData = "Foo"
+
+            let data =
+                Result.Ok innerData
+                |> Async.singleton
+
+            let! actual = asyncResultOption {
+                let! data = data
+                return data
+            }
+
+            let! data = data
+            Expect.equal actual (OkSome innerData) "Should be ok"
+        }
+
+        testCaseAsync "Bind Ok AsyncOption"
+        <| async {
+            let innerData = "Foo"
+
+            let data =
+                Some innerData
+                |> Async.singleton
+
+            let! actual = asyncResultOption {
+                let! data = data
+                return data
+            }
+
+            let! data = data
+            Expect.equal actual (OkSome innerData) "Should be ok"
+        }
+
+        testCaseAsync "Bind Ok AsyncResultOption"
+        <| async {
+            let innerData = "Foo"
+
+            let data = AsyncResultOption.retn innerData
+
+            let! actual = asyncResultOption {
+                let! data = data
+                return data
+            }
+
+            let! data = data
+            Expect.equal actual (OkSome innerData) "Should be ok"
+        }
+
+        testCaseAsync "Bind Async"
+        <| async {
+            let innerData = "Foo"
+            let d = Async.singleton innerData
+
+            let! actual = asyncResultOption {
+                let! data = d
+                return data
+            }
+
+            Expect.equal actual (OkSome innerData) "Should be ok"
+        }
+
+
+#if !FABLE_COMPILER
+        testCaseAsync "Bind Ok TaskResult"
+        <| async {
+            let innerData = "Foo"
+
+            let data =
+                Result.Ok innerData
+                |> Task.FromResult
+
+            let! actual = asyncResultOption {
+                let! data = data
+                return data
+            }
+
+            Expect.equal actual (OkSome innerData) "Should be ok"
+        }
+
+        testCaseAsync "Bind Ok TaskOption"
+        <| async {
+            let innerData = "Foo"
+
+            let data =
+                Some innerData
+                |> Task.FromResult
+
+            let! actual = asyncResultOption {
+                let! data = data
+                return data
+            }
+
+            Expect.equal actual (OkSome innerData) "Should be ok"
+        }
+
+
+        testCaseAsync "Bind Ok TaskResultOption"
+        <| async {
+            let innerData = "Foo"
+
+            let data =
+                Some innerData
+                |> Ok
+                |> Task.FromResult
+
+            let! actual = asyncResultOption {
+                let! data = data
+                return data
+            }
+
+            Expect.equal actual (OkSome innerData) "Should be ok"
+        }
+
+        testCaseAsync "Bind Task Generic"
+        <| async {
+            let innerData = "Foo"
+
+            let! actual = asyncResultOption {
+                let! data = Task.FromResult innerData
+                return data
+            }
+
+            Expect.equal actual (OkSome innerData) "Should be ok"
+        }
+        testCaseAsync "Bind Task"
+        <| async {
+            let innerData = "Foo"
+            let! actual = asyncResultOption { do! Task.FromResult innerData :> Task }
+
+            Expect.equal actual (OkSome()) "Should be ok"
+        }
+#endif
+    ]
+
+
+let ``AsyncResultOptionCE combine/zero/delay/run Tests`` =
+    testList "AsyncResultOptionCE combine/zero/delay/run Tests" [
+        testCaseAsync "Zero/Combine/Delay/Run"
+        <| async {
+            let data = 42
+
+            let! actual = asyncResultOption {
+                let result = data
+
+                if true then
+                    ()
+
+                return result
+            }
+
+            Expect.equal actual (OkSome data) "Should be ok"
+        }
+    ]
+
+let ``AsyncResultOptionCE try Tests`` =
+    testList "AsyncResultOptionCE try Tests" [
+        testCaseAsync "Try With"
+        <| async {
+            let data = 42
+
+            let! actual = asyncResultOption {
+                let data = data
+
+                try
+                    ()
+                with _ ->
+                    ()
+
+                return data
+            }
+
+            Expect.equal actual (OkSome data) "Should be ok"
+        }
+        testCaseAsync "Try Finally"
+        <| async {
+            let data = 42
+
+            let! actual = asyncResultOption {
+                let data = data
+
+                try
+                    ()
+                finally
+                    ()
+
+                return data
+            }
+
+            Expect.equal actual (OkSome data) "Should be ok"
+        }
+    ]
+
+let makeDisposable () =
+    { new System.IDisposable with
+        member this.Dispose() = ()
+    }
+
+
+let ``AsyncResultOptionCE using Tests`` =
+    testList "AsyncResultOptionCE using Tests" [
+        testCaseAsync "use normal disposable"
+        <| async {
+            let data = 42
+
+            let! actual = asyncResultOption {
+                use d = makeDisposable ()
+                return data
+            }
+
+            Expect.equal actual (OkSome data) "Should be ok"
+        }
+        testCaseAsync "use! normal wrapped disposable"
+        <| async {
+            let data = 42
+
+            let! actual = asyncResultOption {
+                use! d =
+                    makeDisposable ()
+                    |> Result.Ok
+
+                return data
+            }
+
+            Expect.equal actual (OkSome data) "Should be ok"
+        }
+#if !FABLE_COMPILER
+        // Fable can't handle null disposables you get
+        // TypeError: Cannot read property 'Dispose' of null
+        testCaseAsync "use null disposable"
+        <| async {
+            let data = 42
+
+            let! actual = asyncResultOption {
+                use d = null
+                return data
+            }
+
+            Expect.equal actual (OkSome data) "Should be ok"
+        }
+#endif
+    ]
+
+
+let ``AsyncResultOptionCE loop Tests`` =
+    testList "AsyncResultOptionCE loop Tests" [
+        testCaseAsync "while"
+        <| async {
+            let data = 42
+            let mutable index = 0
+
+            let! actual = asyncResultOption {
+                while index < 10 do
+                    index <- index + 1
+
+                return data
+            }
+
+            Expect.equal actual (OkSome data) "Should be ok"
+        }
+        yield! [
+            let maxIndices = [
+                10
+                1000000
+            ]
+
+            for maxIndex in maxIndices do
+                testCaseAsync
+                <| sprintf "While - %i" maxIndex
+                <| async {
+                    let data = 42
+                    let mutable index = 0
+
+                    let! actual = asyncResultOption {
+                        while index < maxIndex do
+                            index <- index + 1
+
+                        return data
+                    }
+
+                    Expect.equal index maxIndex "Index should reach maxIndex"
+                    Expect.equal actual (OkSome data) "Should be ok"
+                }
+        ]
+
+        testCaseAsync "while fail"
+        <| async {
+
+            let mutable loopCount = 0
+            let mutable wasCalled = false
+
+            let sideEffect () =
+                wasCalled <- true
+                "ok"
+
+            let expected = Error "NOPE"
+
+            let data = [
+                Ok "42"
+                Ok "1024"
+                expected
+                Ok "1M"
+                Ok "1M"
+                Ok "1M"
+            ]
+
+            let! actual = asyncResultOption {
+                while loopCount < data.Length do
+                    let! x = data.[loopCount]
+
+                    loopCount <-
+                        loopCount
+                        + 1
+
+                return sideEffect ()
+            }
+
+            Expect.equal loopCount 2 "Should only loop twice"
+            Expect.equal actual expected "Should be an error"
+            Expect.isFalse wasCalled "No additional side effects should occur"
+        }
+
+        testCaseAsync "for in"
+        <| async {
+            let data = 42
+
+            let! actual = asyncResultOption {
+                for i in [ 1..10 ] do
+                    ()
+
+                return data
+            }
+
+            Expect.equal actual (OkSome data) "Should be ok"
+        }
+        testCaseAsync "for to"
+        <| async {
+            let data = 42
+
+            let! actual = asyncResultOption {
+                for i = 1 to 10 do
+                    ()
+
+                return data
+            }
+
+            Expect.equal actual (OkSome data) "Should be ok"
+        }
+        testCaseAsync "for in fail"
+        <| async {
+            let mutable loopCount = 0
+            let expected = Error "error"
+
+            let data = [
+                Ok "42"
+                Ok "1024"
+                expected
+                Ok "1M"
+            ]
+
+            let! actual = asyncResultOption {
+                for i in data do
+                    let! x = i
+
+                    loopCount <-
+                        loopCount
+                        + 1
+
+                    ()
+
+                return "ok"
+            }
+
+            Expect.equal 2 loopCount "Should only loop twice"
+            Expect.equal actual expected "Should be and error"
+        }
+    ]
+
+let allTests =
+    testList "AsyncResultCETests" [
+        ``AsyncResultOptionCE return Tests``
+        ``AsyncResultOptionCE return! Tests``
+        ``AsyncResultOptionCE bind Tests``
+        ``AsyncResultOptionCE combine/zero/delay/run Tests``
+        ``AsyncResultOptionCE try Tests``
+        ``AsyncResultOptionCE using Tests``
+        ``AsyncResultOptionCE loop Tests``
+    ]
