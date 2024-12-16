3deb33600a9f4f010dc78ffe38b93032b4b3c590
1eyewonder <bronsonbata@gmail.com>
2024-12-15 21:17:15 -0600

    Updated benchmarks to work with net8.0 updates

--- benchmarks/Program.fs
+++ benchmarks/Program.fs
@@ -1,28 +1,20 @@
-﻿open System
-open BenchmarkDotNet.Running
-open benchmarks
+﻿open BenchmarkDotNet.Running
 open BenchmarkDotNet.Configs
-open BenchmarkDotNet.Jobs
 open BenchmarkDotNet.Columns
-open BenchmarkDotNet.Environments
 open BenchmarkDotNet.Reports
-open FsToolkit.ErrorHandling.Benchmarks
-open ApplicativeTests
+open System.Reflection
+
 
 [<EntryPoint>]
 let main argv =
 
     let cfg =
         DefaultConfig.Instance
-            // .AddJob(Job.Default.WithRuntime(CoreRuntime.Core50))
-            .AddJob(Job.Default.WithRuntime(CoreRuntime.Core70))
             .AddColumn(StatisticColumn.P80, StatisticColumn.P95)
             .WithSummaryStyle(SummaryStyle.Default.WithRatioStyle(RatioStyle.Trend))
-    // BenchmarkRunner.Run<EitherMapBenchmarks>() |> ignore
-    // BenchmarkRunner.Run<TaskResult_BindCEBenchmarks>(cfg) |> ignore
-    // BenchmarkRunner.Run<BindSameBenchmarks>() |> ignore
 
-    BenchmarkRunner.Run<SeqTests.SeqBenchmarks>(cfg, argv)
+    // see here for console args: https://benchmarkdotnet.org/articles/guides/console-args.html
+    BenchmarkRunner.Run(assembly = Assembly.GetExecutingAssembly(), config = cfg, args = argv)
     |> ignore
 
     0 // return an integer exit code
