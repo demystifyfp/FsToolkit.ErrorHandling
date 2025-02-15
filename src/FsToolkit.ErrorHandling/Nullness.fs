a899a02a20d11bc44e6adbf3739ae4fa9b4a7789
Jimmy Byrd <jimmy.byrd@motivity.net>
2025-02-15 16:41:03 -0500

    support fable

--- src/FsToolkit.ErrorHandling/Nullness.fs
+++ src/FsToolkit.ErrorHandling/Nullness.fs
@@ -2,21 +2,21 @@ namespace FsToolkit.ErrorHandling
 open System
 
 type ExceptionNull =
-    #if NET9_0 
+    #if NET9_0 && !FABLE_COMPILER
         Exception | null
     #else
         Exception
     #endif
 
 type IDisposableNull =
-    #if NET9_0 
+    #if NET9_0 && !FABLE_COMPILER
         IDisposable | null
     #else
         IDisposable
     #endif
 
 type IAsyncDisposableNull =
-    #if NET9_0 
+    #if NET9_0 && !FABLE_COMPILER
         IAsyncDisposable | null
     #else
         IAsyncDisposable
