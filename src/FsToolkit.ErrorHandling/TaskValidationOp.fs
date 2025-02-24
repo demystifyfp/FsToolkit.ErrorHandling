d6ce33b83ebbee1d663629e74576cb70a44ed303
Matthew Watt <matt@twopoint.dev>
2025-02-23 20:51:04 -0600

    Implement TaskValidationOp functions

---
+++ src/FsToolkit.ErrorHandling/TaskValidationOp.fs
@@ -0,0 +1,35 @@
+﻿namespace FsToolkit.ErrorHandling.Operator.TaskValidation
+
+open FsToolkit.ErrorHandling
+
+[<AutoOpen>]
+module TaskValidation =
+    let inline (<!>)
+        ([<InlineIfLambda>] mapper: 'okInput -> 'okOutput)
+        (input: TaskValidation<'okInput, 'error>)
+        : TaskValidation<'okOutput, 'error> =
+        TaskValidation.map mapper input
+
+    let inline (<!^>)
+        ([<InlineIfLambda>] mapper: 'okInput -> 'okOutput)
+        (input: Result<'okInput, 'error>)
+        : TaskValidation<'okOutput, 'error> =
+        TaskValidation.map mapper (TaskValidation.ofResult input)
+
+    let inline (<*>)
+        (applier: TaskValidation<('okInput -> 'okOutput), 'error>)
+        (input: TaskValidation<'okInput, 'error>)
+        : TaskValidation<'okOutput, 'error> =
+        TaskValidation.apply applier input
+
+    let inline (<*^>)
+        (applier: TaskValidation<('okInput -> 'okOutput), 'error>)
+        (input: Result<'okInput, 'error>)
+        : TaskValidation<'okOutput, 'error> =
+        TaskValidation.apply applier (TaskValidation.ofResult input)
+
+    let inline (>>=)
+        (input: TaskValidation<'okInput, 'error>)
+        ([<InlineIfLambda>] binder: 'okInput -> TaskValidation<'okOutput, 'error>)
+        : TaskValidation<'okOutput, 'error> =
+        TaskValidation.bind binder input
