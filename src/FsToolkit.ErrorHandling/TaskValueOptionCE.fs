1942e93ca0b576286af284429cea05f272d3af6b
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2026-04-21 10:23:17 +0200

    Fix exceptions handling in task dynamic invocation

--- src/FsToolkit.ErrorHandling/TaskValueOptionCE.fs
+++ src/FsToolkit.ErrorHandling/TaskValueOptionCE.fs
@@ -306,8 +306,7 @@ type TaskValueOptionBuilder() =
                         // If the `sm.Data.MethodBuilder` has already been set somewhere else (like While/WhileDynamic), we shouldn't continue
                         if sm.Data.IsTaskCompleted then
                             ()
-
-                        if step then
+                        elif step then
                             sm.Data.SetResult()
                         else
                             match sm.ResumptionDynamicInfo.ResumptionData with
